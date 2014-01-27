b42ba0826430b56e53b4951d397363c2631079c9
wolfg1969 <wolfg1969@gmail.com>
2014-01-27 21:31:58 +0800

    eb sample app

---
+++ ebsample/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('ebsample',
+    # Examples:
+    # url(r'^$', 'ebsample.views.home', name='home'),
+    # url(r'^ebsample/', include('ebsample.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'views.index', name='index'),
+)

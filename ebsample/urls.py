2fd000865d8aedd6af15dd050f41930e98bd1467
wolfg1969 <wolfg1969@gmail.com>
2016-07-02 09:08:08 +0800

    add a static homepage

--- ebsample/urls.py
+++ ebsample/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from ebsample_app import views
+
 urlpatterns = [
+    url(r'', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
 ]

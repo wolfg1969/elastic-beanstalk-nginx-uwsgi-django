1853592c377cf04a1653dabdeca30d713ce5174f
wolfg1969 <wolfg1969@gmail.com>
2017-04-07 14:01:58 +0800

    new homepage

--- ebsample/urls.py
+++ ebsample/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from ebsample_app import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', TemplateView.as_view(template_name="home.html")),
     url(r'^admin/', admin.site.urls),
 ]
